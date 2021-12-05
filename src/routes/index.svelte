78a74c2f6d29be35e15098f539112c29bcfb1b88
jacktanlol <ejacktanjack@gmail.com>
2021-12-05 06:50:17 +0000

    Supabase-login

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,14 @@
     import Greeting from '$lib/Greeting.svelte';
     import Entry from '$lib/Entry.svelte';
     import EntryModal from '$lib/EntryModal.svelte'
+    import supabase from '$lib/db';
+    async function signOut() {
+    const { error } = await supabase.auth.signOut({
+        email
+    });
+
+    if (error) alert(error.message); // alert if error
+}
 </script>
 
 
@@ -9,14 +17,16 @@
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal"
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal" 
         data-bs-target="#newEntry" />
     </div>
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
     	<Entry />
-   	 
+   	 <section class="container px-4 py-3 text-center">
+        <button class="btn btn-secondary" on:click={signOut}>Logout</button>
+    </section>
     </div>
 </section>
 <EntryModal/>
